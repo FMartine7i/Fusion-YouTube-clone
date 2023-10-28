fec0c6d2267da80ba92c0b5d8d303db4e847cca0
FMartine7i <frmmartinetti98@gmail.com>
2023-10-28 20:34:47 -0300

    video container finalizado

--- project/main.js
+++ project/main.js
@@ -3,13 +3,22 @@ const sidebar = document.querySelector('.sidebar')
 const showSubs = document.querySelector('.show-subs')
 const showSideBar = document.querySelector('.menu-sidebar')
 const subsList = document.querySelector('.subscription-list')
+const mainContainer = document.querySelector('.main-container')
 
 
 showSideBar.addEventListener('click', () => {
     sidebar.classList.toggle("small-sidebar");
+
+    if(sidebar.classList.contains('small-sidebar')) {
+        mainContainer.style.paddingLeft = "7%";
+    }
+    else{
+        mainContainer.style.paddingLeft = "17%";
+    }
 })
 
 showSubs.addEventListener('click', () => {
     showSubs.classList.toggle('title-change');
     subsList.classList.toggle("show-subs-list");
 })
+
